9a828b88b0a9be2489aa845ce8dbf3323f57f7cd
vpflieger <verena.pflieger@inwt-statistics.de>
2018-06-27 14:33:18 +0200

    adding dataset for clv prediction

---
+++ MarketingAnalytics/R/clvData.R
@@ -0,0 +1,67 @@
+#' Customer Relation Management Data
+#'
+#' Dataset on behavior of customers of an online shop for one year with 15 variables:
+#' \describe{
+#' \item{\code{customerID}}{identifier for customer}
+#' \item{\code{nOrders}}{number of orders}
+#' \item{\code{nItems}}{number of items}
+#' \item{\code{daysSinceLastOrder}}{average days between orders}
+#' \item{\code{margin}}{margin generated in the current year}
+#' \item{\code{returnRatio}}{ratio of returned articles}
+#' \item{\code{shareOwnBrand}}{share of own brand}
+#' \item{\code{shareVoucher}}{share of  vouchers used}
+#' \item{\code{shareSale}}{share of articles on sale}
+#' \item{\code{gender}}{gender}
+#' \item{\code{age}}{age}
+#' \item{\code{marginPerOrder}}{margin per order}
+#' \item{\code{marginPerItem}}{margin per item}
+#' \item{\code{itemsPerOrder}}{number of items per order}
+#' \item{\code{futureMargin}}{margin generated in the following year}
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(clvData1)
+#'
+#' @format An object of class \code{"csv"}
+#'
+#' @keywords datasets CLV
+#'
+#' @source INWT Statistics GmbH
+#'
+#'
+"clvData1"
+
+
+
+
+#' Dataset on behavior of customers of an online shop for one year with 14 variables:
+#' \describe{
+#' \item{\code{customerID}}{identifier for customer}
+#' \item{\code{nOrders}}{number of orders}
+#' \item{\code{nItems}}{number of items}
+#' \item{\code{daysSinceLastOrder}}{average days between orders}
+#' \item{\code{margin}}{margin generated in the current year}
+#' \item{\code{returnRatio}}{ratio of returned articles}
+#' \item{\code{shareOwnBrand}}{share of own brand}
+#' \item{\code{shareVoucher}}{share of  vouchers used}
+#' \item{\code{shareSale}}{share of articles on sale}
+#' \item{\code{gender}}{gender}
+#' \item{\code{age}}{age}
+#' \item{\code{marginPerOrder}}{margin per order}
+#' \item{\code{marginPerItem}}{margin per item}
+#' \item{\code{itemsPerOrder}}{number of items per order}
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(clvData2)
+#'
+#' @format An object of class \code{"csv"}
+#'
+#' @keywords datasets CLV
+#'
+#' @source INWT Statistics GmbH
+#'
+#'
+"clvData2"
